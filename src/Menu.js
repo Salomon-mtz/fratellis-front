0b4dda85854a7690d03785db06eca638c4ee4db5
SalomonMartz <mtzelisalomon@gmail.com>
2022-02-06 15:42:03 -0600

    Add of logo in navbar

--- src/Menu.js
+++ src/Menu.js
@@ -1,8 +1,12 @@
+import logo from './logo.png';
+
 function Menu(){
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-center">
       <div className="container-fluid">
-        <a className="navbar-brand" href="./home"><img src="Imagen/logo.png" class="img-fluid" alt="Responive"></img></a>
+        <a className="navbar-brand" href="./home">
+          <img src={logo} className="fratellis-logo" alt="Responive" />
+        </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
