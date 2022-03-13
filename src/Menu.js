b08d17c4279e44502f7fd3b01747b2f957be084a
Andrés Cidel <andres@cidel.com.mx>
2022-03-13 22:16:51 +0100

    Improve navbar CSS

--- src/Menu.js
+++ src/Menu.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Menu(){
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark custom-menu">
       <div className="container-fluid justify-content-end">
         <Link className="navbar-brand" to="/">
           <Logo width="40px" />
@@ -11,8 +11,8 @@ function Menu(){
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav center me-auto mb-2 mb-lg-0">
+        <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+          <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="/">Home</Link>
             </li>
