b763e86c6f5445aebed548e118b6b25ad3de1651
Andrés Cidel <andres@cidel.com.mx>
2022-03-20 22:42:26 +0100

    Create custom link component

--- src/Menu.js
+++ src/Menu.js
@@ -1,6 +1,21 @@
 import { ReactComponent as Logo } from './fratellis.svg'
-import { Link } from 'react-router-dom';
+import { Link, useResolvedPath, useMatch } from 'react-router-dom';
 import { useState } from 'react';
+
+function CustomLink({ children, to, ...props }) {
+  let resolved = useResolvedPath(to);
+  let match = useMatch({ path: resolved.pathname, end: true });
+
+  return (
+      <Link
+        className={`nav-link ${match ? "active" : ""}`}
+        to={to}
+        {...props}
+      >
+        {children}
+      </Link>
+  );
+}
 
 function Menu(){
   const [collapsed, toggleCollapsed] = useState(false);
@@ -15,22 +30,22 @@ function Menu(){
         <Link className="navbar-brand" to="/">
           <Logo width="40px" />
         </Link>
-        <button onClick={handleToggle} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button onClick={handleToggle} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#fratellisMenu" aria-controls="fratellisMenu" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className={`navbar-collapse justify-content-center collapse ${collapsed ? "show" : ""}`} id="navbarSupportedContent">
+        <div className={`navbar-collapse justify-content-center collapse ${collapsed ? "show" : ""}`} id="fratellisMenu">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              <CustomLink to="/">Home</CustomLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/paquetes">Paquetes</Link>
+              <CustomLink to="/paquetes">Paquetes</CustomLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/nosotros">Nosotros</Link>
+              <CustomLink to="/nosotros">Nosotros</CustomLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/contacto">Contacto</Link>
+              <CustomLink to="/contacto">Contacto</CustomLink>
             </li>
           </ul>
         </div>
